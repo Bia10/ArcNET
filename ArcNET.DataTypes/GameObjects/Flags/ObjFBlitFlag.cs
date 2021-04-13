cabbe4003f883c325ef0a160d5288118be99e39a
Bia10 <t.locke1234@gmail.com>
2021-04-14 01:40:00 +0200

    add obj flags

---
+++ ArcNET.DataTypes/GameObjects/Flags/ObjFBlitFlag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ArcNET.DataTypes.GameObjects.Flags
+{
+    [Flags]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public enum ObjFBlitFlag
+    {
+        TAB_BLEND_ADD,
+    }
+}
