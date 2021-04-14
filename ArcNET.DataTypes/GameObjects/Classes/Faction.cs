5fb3187e9b809d9e382ab30265fc6081bfaf1b9a
Bia10 <t.locke1234@gmail.com>
2021-04-14 19:31:25 +0200

    add AIPacket, Faction classes

---
+++ ArcNET.DataTypes/GameObjects/Classes/Faction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArcNET.DataTypes.GameObjects.Classes
+{
+    public class Faction
+    {
+        public List<Tuple<int, string>> Entries;
+    }
+}
