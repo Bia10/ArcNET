3070ed8c60ee7136eea5b43b654d25b60bd28518
Bia10 <t.locke1234@gmail.com>
2021-04-14 17:46:09 +0200

    add invSource classes

---
+++ ArcNET.DataTypes/GameObjects/Classes/InventorySource.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ArcNET.DataTypes.GameObjects.Classes
+{
+    public class InventorySource
+    {
+        public class InventorySourceEntry
+        {
+            public double Chance; //TODO: the values are a mess .... 0.0%,-10%,500%?
+            public int PrototypeId;
+        }
+
+        public int Id; //0 reserved
+        public string Name;
+        public List<InventorySourceEntry> Entries;
+    }
+}
---
+++ ArcNET.DataTypes/GameObjects/Classes/InventorySourceBuy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ArcNET.DataTypes.GameObjects.Classes
+{
+    public class InventorySourceBuy
+    {
+        public class InventorySourceBuyEntry
+        {
+            public int PrototypeId;
+        }
+
+        public int Id; //0 reserved
+        public string Name;
+        public List<InventorySourceBuyEntry> Entries; //TODO: Can be just {all}
+    }
+}
