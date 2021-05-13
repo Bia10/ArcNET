bd28ac4e8d89628124383cea0700674a48540499
Bia10 <t.locke1234@gmail.com>
2021-05-13 17:51:59 +0200

    continue refactors

--- ArcNET.Utilities/GitHub.cs
+++ ArcNET.Utilities/GitHub.cs
@@ -2,34 +2,12 @@
 using System.IO;
 using System.Linq;
 using Utils.Console;
+using Utils.FileSystem;
 
 namespace ArcNET.Utilities
 {
     public static class GitHub
     {
-        private static void Copy(string sourceDir, string targetDir)
-        {
-            if (!Directory.Exists(targetDir))
-                Directory.CreateDirectory(targetDir);
-
-            foreach (var file in Directory.GetFiles(sourceDir))
-                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)));
-            foreach (var directory in Directory.GetDirectories(sourceDir))
-                Copy(directory, Path.Combine(targetDir, Path.GetFileName(directory)));
-        }
-
-        private static void DeleteReadOnly(this FileSystemInfo fileSystemInfo)
-        {
-            if (fileSystemInfo is DirectoryInfo directoryInfo)
-            {
-                foreach (var childInfo in directoryInfo.GetFileSystemInfos())
-                    childInfo.DeleteReadOnly();
-            }
-
-            fileSystemInfo.Attributes = FileAttributes.Normal;
-            fileSystemInfo.Delete();
-        }
-
         public static void CloneHighResPatch(string resultLocation)
         {
             const string repo = "https://github.com/ArcNET-Modding/HighResPatch.git";
@@ -40,12 +18,11 @@ namespace ArcNET.Utilities
 
             ConsoleExtensions.Log($"Cloned {files.Count} files", "success");
             var highResPath = Path.Combine(resultLocation, "HighRes");
-            Copy(highResPath, resultLocation);
+            new DirectoryInfo(highResPath).CopyTo(resultLocation);
 
             var foldersToDelete = new DirectoryInfo(highResPath);
             ConsoleExtensions.Log($"Removing empty folder: {foldersToDelete}", "info");
-            if (foldersToDelete.Exists)
-                DeleteReadOnly(foldersToDelete);
+            foldersToDelete.ExistsDelete();
         }
     }
 }
